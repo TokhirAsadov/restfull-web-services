da1e7ed5ce8c4e7ae7f019ecb822acd791036231
Bismillah <guvalakat1603@gmail.con>
2023-08-10 01:51:00 +0500

    Media type versioning (a.k.a "content negotiation" or "accept header") - GitHub

--- src/main/java/com/tohir/webservices/restfullwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/tohir/webservices/restfullwebservices/versioning/VersioningPersonController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersioningPersonController {
 
+    // URL Versioning - Twitter
     @GetMapping("/v1/person")
     public PersonV1 getFirstVersioningOfPerson(){
         return new PersonV1("Tohir Asadov");
@@ -16,6 +17,7 @@ public class VersioningPersonController {
         return new PersonV2(new Name("Tohir","Asadov"));
     }
 
+    // Request Parameter versioning - Amazon
     @GetMapping(path = "/person",params = "version=1")
     public PersonV1 getFirstVersioningOfPersonRequestParameter(){
         return new PersonV1("Tohir Asadov");
@@ -26,12 +28,23 @@ public class VersioningPersonController {
         return new PersonV2(new Name("Tohir","Asadov"));
     }
 
+    // (Custom) headers versioning - Microsoft
     @GetMapping(path = "/person/header",headers = "X-API-VERSION=1")
     public PersonV1 getFirstVersioningOfPersonRequestHeader(){
         return new PersonV1("Tohir Asadov");
     }
     @GetMapping(path = "/person/header",headers = "X-API-VERSION=2")
     public PersonV2 getSecondVersioningOfPersonRequestHeader(){
+        return new PersonV2(new Name("Tohir","Asadov"));
+    }
+
+    // Media type versioning (a.k.a "content negotiation" or "accept header") - GitHub
+    @GetMapping(path = "/person/accept",produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersioningOfPersonAcceptHeader(){
+        return new PersonV1("Tohir Asadov");
+    }
+    @GetMapping(path = "/person/accept",produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersioningOfPersonAcceptHeader(){
         return new PersonV2(new Name("Tohir","Asadov"));
     }
 }
