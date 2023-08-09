5b4f9d6a648610622f8dacacb72eb16cb68bc1bf
Bismillah <guvalakat1603@gmail.con>
2023-08-10 01:18:35 +0500

    request parameter versioning - Amazon

--- src/main/java/com/tohir/webservices/restfullwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/tohir/webservices/restfullwebservices/versioning/VersioningPersonController.java
@@ -7,12 +7,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class VersioningPersonController {
 
     @GetMapping("/v1/person")
-    public PersonV1 getFirstVersioningPerson(){
+    public PersonV1 getFirstVersioningOfPerson(){
         return new PersonV1("Tohir Asadov");
     }
 
     @GetMapping("/v2/person")
-    public PersonV2 getSecondVersioningPerson(){
+    public PersonV2 getSecondVersioningOfPerson(){
+        return new PersonV2(new Name("Tohir","Asadov"));
+    }
+
+    @GetMapping(path = "/person",params = "version=1")
+    public PersonV1 getFirstVersioningOfPersonRequestParameter(){
+        return new PersonV1("Tohir Asadov");
+    }
+
+    @GetMapping(path = "/person",params = "version=2")
+    public PersonV2 getSecondVersioningOfPersonRequestParameter(){
         return new PersonV2(new Name("Tohir","Asadov"));
     }
 }
