4b1db95a19ff484c5a077ff2009fe1c3cef621b3
TokhirAsadov <guvalakat1603@gmail.com>
2023-12-24 16:38:48 +0500

    @JsonFilter provides dynamic filtering,also we need to bring name (such as 'SomeBeanFilter') to class that it was used.

--- src/main/java/com/tohir/webservices/restfullwebservices/filtering/FilteringController.java
+++ src/main/java/com/tohir/webservices/restfullwebservices/filtering/FilteringController.java
@@ -1,5 +1,10 @@
 package com.tohir.webservices.restfullwebservices.filtering;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.PropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +14,16 @@ import java.util.List;
 @RestController
 public class FilteringController {
 
-    @GetMapping("/filtering")
-    public SomeBean filtering() {
-        return new SomeBean("field1","field2","field3");
+    @GetMapping("/filtering")//field2 will have to ignored
+    public MappingJacksonValue filtering() {
+        SomeBean someBean = new SomeBean("field1", "field2", "field3");
+
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
+        PropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1","field3");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter",filter);
+        mappingJacksonValue.setFilters(filters);
+
+        return mappingJacksonValue;
     }
 
     @GetMapping("/filtering-list")
