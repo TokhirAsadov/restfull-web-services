b4f992c40ea971b0b0d8081104cdf41206b166d1
TokhirAsadov <guvalakat1603@gmail.com>
2023-12-24 16:23:49 +0500

    @JsonIgnore, @JsonIgnoreProperties provide static filtering

--- src/main/java/com/tohir/webservices/restfullwebservices/filtering/SomeBean.java
+++ src/main/java/com/tohir/webservices/restfullwebservices/filtering/SomeBean.java
@@ -3,11 +3,11 @@ package com.tohir.webservices.restfullwebservices.filtering;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-//@JsonIgnoreProperties("field1")// field1 was ignored that we never get it when we send request
+//@JsonIgnoreProperties("field1")// field1 was ignored that we never get it when we send request (*) static filtering
 public class SomeBean {
     private String field1;
 
-    //@JsonIgnore // field2 was ignored that we never get it when we send request
+    //@JsonIgnore // field2 was ignored that we never get it when we send request (*) static filtering
     private String field2;
     private String field3;
 
