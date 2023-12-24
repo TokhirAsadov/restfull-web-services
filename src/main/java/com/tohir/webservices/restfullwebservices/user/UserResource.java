ec23b0a13bb7197e7d3daccd129dc1d93059ee76
TokhirAsadov <guvalakat1603@gmail.com>
2023-12-24 15:29:44 +0500

    how to use EntityModel and WebMvcBuilder with HATEOAS?

--- src/main/java/com/tohir/webservices/restfullwebservices/user/UserResource.java
+++ src/main/java/com/tohir/webservices/restfullwebservices/user/UserResource.java
@@ -1,7 +1,11 @@
 package com.tohir.webservices.restfullwebservices.user;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*; // for to use universally
+
 import com.tohir.webservices.restfullwebservices.exceptions.UserNotFoundException;
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -23,12 +27,34 @@ public class UserResource {
         return service.findAll();
     }
 
+
+    // http://localhost:8080/users
+
+    //EntityModel
+    //WebMvcBuilder
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable Integer id) {
+    public EntityModel<User> retrieveUser(@PathVariable Integer id) {
         User user = service.findOne(id);
         if (user == null)
             throw new UserNotFoundException("id: " + id);
-        return user;
+
+
+        /**
+         * {
+         *      "id":1,
+         *      "name":"John",
+         *      "birthDate":"1993-12-24",
+         *      "_links":{
+         *          "all-users":{
+         *              "href":"http://localhost:8080/users"
+         *          }
+         *      }
+         * }
+         * **/
+        EntityModel<User> entityModel = EntityModel.of(user);// for add pretty looks as above
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());// link qushish uchun ishlatiladi
+        entityModel.add(link.withRel("all-users"));// name of link was added
+        return entityModel;
     }
 
     @DeleteMapping("/users/{id}")
