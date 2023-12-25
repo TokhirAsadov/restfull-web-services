1532102b1f8318a53f6042f875371a901c75fd93
TokhirAsadov <guvalakat1603@gmail.com>
2023-12-26 03:54:02 +0500

    add data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,15 @@ values(10003, current_date(),'Xo`jayor');
 
 insert into user_details(id,birth_date,name)
 values(10004, current_date(),'Kamron');
+
+insert into post(id,description,user_id)
+values (20001,'I want to learn JAVA',10001);
+
+insert into post(id,description,user_id)
+values (20002,'I want to learn Spring Boot 3',10001);
+
+insert into post(id,description,user_id)
+values (20003,'I want to learn NextJS',10002);
+
+insert into post(id,description,user_id)
+values (20004,'I want to learn NodeJS',10002);
