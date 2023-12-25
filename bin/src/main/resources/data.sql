42f781e988c2e355d55b058bee061b0f12a2684b
TokhirAsadov <guvalakat1603@gmail.com>
2023-12-26 03:53:31 +0500

    add to git

---
+++ bin/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into user_details(id,birth_date,name)
+values(10001, current_date(),'Tohir');
+
+insert into user_details(id,birth_date,name)
+values(10002, current_date(),'Bahodir');
+
+insert into user_details(id,birth_date,name)
+values(10003, current_date(),'Xo`jayor');
+
+insert into user_details(id,birth_date,name)
+values(10004, current_date(),'Kamron');
