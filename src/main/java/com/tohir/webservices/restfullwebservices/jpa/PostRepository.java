0ac635db966615596b0e57dd067e8d736217161b
TokhirAsadov <guvalakat1603@gmail.com>
2023-12-26 04:05:56 +0500

    post repository was created

---
+++ src/main/java/com/tohir/webservices/restfullwebservices/jpa/PostRepository.java
@@ -0,0 +1,7 @@
+package com.tohir.webservices.restfullwebservices.jpa;
+
+import com.tohir.webservices.restfullwebservices.user.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post,Integer> {
+}
